513586e545f5de9fdd4b77273a353bbc55689865
luohuidong <luohuidong01@126.com>
2023-08-23 00:35:47 +0800

    build: modify build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'node:path'
+
+import packagejson from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/lib/index.ts'),
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: Object.keys(packagejson.peerDependencies)
+    }
+  }
 })
